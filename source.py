2fb77320f456b39929a9cdeaa052d622f033edb7
atnzpe <gleysonasilva@gmail.com>
2022-11-17 21:25:37 -0300

    Adiciona biblioteca random

--- source.py
+++ source.py
@@ -1,7 +1,9 @@
+import random
+
 class Bicicleta:
     def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
-        self.modelo = modelo
+        self.modelo = modelogit
         self.ano = ano
         self.valor = valor
 
